e9c88e9f80733c43fae95e13e0a2e49fc867c72d
aashimgarg <18bcs001@smvdu.ac.in>
2020-09-16 15:52:22 +0530

    added parsing incoming requests

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const app = express();
+const bodyParser = require('body-parser')
 
-app.use( (req,res) => {
+app.use(bodyParser.urlencoded({
+    extended:false
+}));
+
+app.use('/add-product' , (req,res) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product </button></form>')
+})
+
+app.use('/product' , (req,res) => {
+    console.log(req.body)
+    res.redirect('/')
+})
+
+app.use('/' , (req,res) => {
     res.send('<h1> Hello there </h1>')
 })
+
 
 app.listen(3000)
