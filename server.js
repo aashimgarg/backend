6358c3007941c508be5109f0574488c86432e99b
aashimgarg <18bcs001@smvdu.ac.in>
2020-09-16 17:24:52 +0530

    added modular routing

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser')
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({
     extended:false
 }));
 
-app.use('/add-product' , (req,res) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product </button></form>')
-})
-
-app.use('/product' , (req,res) => {
-    console.log(req.body)
-    res.redirect('/')
-})
-
-app.use('/' , (req,res) => {
-    res.send('<h1> Hello there </h1>')
-})
-
+app.use(shopRoutes)
+app.use(adminRoutes)
 
 app.listen(3000)
