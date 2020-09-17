b952300e78b3c0709508cf1d1e6b85b6b4731bcd
aashimgarg <18bcs001@smvdu.ac.in>
2020-09-17 21:28:57 +0530

    added a error 404 controller

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const bodyParser = require('body-parser')
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
+const errorController = require('./controllers/error')
+
 app.use(bodyParser.urlencoded({
     extended:false
 }));
@@ -17,8 +19,6 @@ app.use(express.static(path.join(__dirname,'public')))
 app.use(shopRoutes)
 app.use('/admin',adminRoutes)
 
-app.use((req,res) => {
-    res.status(404).render('404')
-})
+app.use(errorController.get404)
 
-app.listen(3030)
+app.listen(3000)
