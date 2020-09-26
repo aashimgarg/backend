26fd97bb7bb598504d59fe668562b806b50750ae
aashimgarg <18bcs001@smvdu.ac.in>
2020-09-27 00:27:44 +0530

    displaying the orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -99,3 +99,15 @@ exports.getIndex = (req, res, next) => {
       .catch(err => console.log(err));
   };
   
+  exports.getOrders = (req, res, next) => {
+    Order.find({ 'user.userId': req.user._id })
+      .then(orders => {
+        res.render('shop/orders', {
+          path: '/orders',
+          pageTitle: 'Your Orders',
+          orders: orders
+        });
+      })
+      .catch(err => console.log(err));
+  };
+  
